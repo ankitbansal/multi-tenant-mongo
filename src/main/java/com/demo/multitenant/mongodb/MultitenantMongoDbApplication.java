bf31a344237b66650b470b9a48569c3f6cd2d154
Ankit Bansal <bansal.ankit@infoedge.com>
2020-07-18 09:50:08 +0530

    created base project

---
+++ src/main/java/com/demo/multitenant/mongodb/MultitenantMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.demo.multitenant.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenantMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultitenantMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/multitenant/mongodb/MultitenantMongoDbApplicationTest.java
@@ -0,0 +1,13 @@
+package com.demo.multitenant.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenantMongoDbApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
